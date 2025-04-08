fff19d5dbf772cc596579785f2370e5c63663f6a
Keanu <145512278+Bassonk376722324841@users.noreply.github.com>
2025-04-08 12:13:51 +1200

    Finished making the function for string scrambles

---
+++ 03_word_scrambler.py
@@ -0,0 +1,21 @@
+import random
+
+def scramble(text):
+    # create list of random characters from the string
+    scramble_list = random.sample(text, len(text))
+
+    # Join as a new string
+    scrambled_text = "".join(scramble_list)
+    return scrambled_text
+
+
+# Example Usage (using a variety of words
+term1 = "Photosynthesis"
+term2 = "Nuclear Fission"
+term3 = "Mitosis"
+term4 = "Erosion"
+
+term_list = [term1,term2,term3,term4]
+
+for term in term_list:
+    print(f"{term} --> {scramble(term)}\n")
